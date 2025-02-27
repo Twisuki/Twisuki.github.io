351ff47a786989a98bf3010288a3bfb4261f8610
Twisuki <Twisuki@outlook.com>
2025-02-27 13:23:42 +0800

    笔记

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -6,8 +6,8 @@ export default defineUserConfig({
   base: "/",
 
   lang: "zh-CN",
-  title: "博客演示",
-  description: "vuepress-theme-hope 的博客演示",
+  title: "Twisuki",
+  description: "Twisuki的个人主页",
 
   theme,
 
