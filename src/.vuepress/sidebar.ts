0a603595f99f9c1deac6e7eb48d1bb64d06dda17
Twisuki <Twisuki@outlook.com>
2025-03-09 14:21:03 +0800

    logo

--- src/.vuepress/sidebar.ts
+++ src/.vuepress/sidebar.ts
@@ -12,25 +12,28 @@ export default sidebar({
       children: [
         {
           text: "开发",
-          prefix: "dev/",
           icon: "gear",
+          prefix: "dev/",
           collapsible: true,
           children: "structure"
         },
         {
           text: "学习",
+          icon: "pen-to-square",
           prefix: "study/",
           collapsible: true,
           children: "structure"
         },
         {
           text: "生活",
+          icon: "hugeicons:bow-tie",
           prefix: "life/",
           collapsible: true,
           children: "structure"
         },
         {
           text: "其他",
+          icon: "fa6-brands:markdown",
           prefix: "other/",
           collapsible: true,
           children: "structure"
@@ -41,6 +44,7 @@ export default sidebar({
       text: "工具 kites",
       icon: "gears",
       prefix: "kits/",
+      collapsible: true,
       children: "structure",
     },
     "friend"
