c1d758ac83a8a6a112df69b1f469e55b04b5622d
Twisuki <Twisuki@outlook.com>
2025-03-24 22:06:18 +0800

    友链氯喵改名

--- src/.vuepress/friends.ts
+++ src/.vuepress/friends.ts
@@ -3,7 +3,7 @@ import { ThemeBlogHomeProjectOptions } from 'vuepress-theme-hope'
 const friends: ThemeBlogHomeProjectOptions[] = [
   {
     name: "NyaCl 氯喵",
-    icon: "https://github.com/NyaCl.png",
+    icon: "https://github.com/AgxCOy.png",
     desc: "氯离子工作室室长, 非常可爱的大姐姐",
     link: "https://nyacl.shimakaze.org/",
   },
